4aeee92bc8e477771458337c6a7599f3bdfe30dd
root <vwbusguy@fedoraproject.org>
2014-06-01 14:30:07 -0700

    Added GitHub link to default login.php

--- login.php
+++ login.php
@@ -9,6 +9,7 @@ require_once('includes/head.php');
 <div class="gray login-container">
 
 <h1 id="login-title">FCC Worship Scheduling Center</h1>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Just passing by?  Get the code on <a href="https://github.com/vwbusguy/band-schedule">GitHub</a>.</p>
 
 <?php if (isset($_GET['logout'])){
 	echo "<p id=\"logged-out\">User " . $_GET['logout'] . " has been logged out.</p>";
@@ -20,5 +21,6 @@ require_once('includes/head.php');
 <input id="password" type="password" name="password" placeholder="Password"><br/>
 <button class="btn btn-large btn-primary" type="submit">Sign In</button>
 </form>
+
 </div>
 <?php require_once('includes/footer.php');?>
